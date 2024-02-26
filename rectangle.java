ec254190748349de0c532ad86311d16889a8b3f7
IanKalotkin <114188361+IanK8@users.noreply.github.com>
2024-02-26 14:36:38 -0500

    the

---
+++ rectangle.java
@@ -0,0 +1,17 @@
+/*******
+ * rectangle
+ * Ian Kalotkin
+ * 2/26/2024
+ ******/
+public class rectangle
+{
+    public static void main(String[]args){
+        System.out.println("***************");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("***************");
+    }
+}
